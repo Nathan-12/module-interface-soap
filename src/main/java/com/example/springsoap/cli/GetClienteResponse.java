26d3a5649818149e99934be14c70aa87e6b89f5d
Nathan Lima Batista <nathanlima.b@gmail.com>
2024-09-28 15:19:49 -0300

    fix: ajustes no dockerfile

--- src/main/java/com/example/springsoap/cli/GetClienteResponse.java
+++ src/main/java/com/example/springsoap/cli/GetClienteResponse.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Eclipse Implementation of JAXB, v3.0.0 
 // Consulte https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2024.09.28 às 02:44:12 PM BRT 
+// Gerado em: 2024.09.28 às 03:18:29 PM BRT 
 //
 
 
